99511fd2f8ca225318cbed88fa524b9398a06d69
Aaron Morris <aaron.r.morris3@gmail.com>
2022-08-19 14:52:39 -0500

    Iterating Arrays

--- js/arrays-lecture.js
+++ js/arrays-lecture.js
@@ -1,11 +1,99 @@
 //declaring an array literal:
 // each item in an array is called an element.
+//
+// let grocerylist = ['bread', 'eggs', 'butter', 'milk'];
+//
+// for(let i = 0; i < grocerylist.length; i++)
+//     if (i === grocerylist.length - 1){
+//     console.log("I have to buy " + grocerylist[i] + " . ");
+// } else {
+//         console.log(" I have to buy " + groceryList[i] + " and ");
+//     }
 
-let grocerylist = ['bread', 'eggs', 'butter', 'milk'];
+// let basicColors = [ 'red', 'blue', 'green', 'yellow'];
+//     for(let i = 0; i < basicColors.length; i++) {
+//         if (i % 2 === 1){
+//      
+//         console.log(basicColors[i]);
+//     }
+//        
+//    function outputArray(array){
+//         for(let i = 0; i < array.length; i++){
+//             console.log(array[i]);
+//    }
+//     }
+//         //outputArray(basicColors);
+// True anoymous function
+ let prices = [32.99, 21.99, 6.99, 4.99, 12.99, 8.98, 5.99];
+         prices.forEach(function(price, index) {
+                 console.log('item number ' + (index + 1)
+                 'costs' + price
+         )
 
-for(let i = 0; i < grocerylist.length; i++)
-    if (i === grocerylist.length - 1){
-    console.log("I have to buy " + grocerylist[i] + " . ");
-} else {
-        console.log(" I have to buy " + groceryList[i] + " and ");
-    }
+         });
+//
+// forEach loop takes an anonymous function
+// the function we pass to the forEach loop has up to three parameters
+// The first parameter represents the array elements
+// the second parameter represents the element index
+// the third parameter represents the array itself
+// the second and third parameters are optional
+// since the function is anonymous, we can use arrow syntax
+
+
+prices.forEach(price => {
+        console.log(price)
+
+        // .push adds to the array
+groceryList.push("potatoes");
+        console.log(groceryList);
+        // .pop removes last item in the array
+        groceryList(List.pop();
+        console.log(groceryList;
+        // .unshift adds to item in front of the array
+        groceryList.unshift("sliced cheddar");
+        console.log(groceryList);
+        // shift removes item from begining of element.
+        groceryList.shift();
+
+ let indexOfButter = groceryList.indexOf("butter");
+ console.log(indexOfButter);
+
+ groceryList[indexOfButter] = "peanut butter";
+ console.log(groceryList);
+
+ let firstHalfOfGroceryList = groceryList.slice(0, 2);
+ console.log(FirstHalfOfGroveryList);
+ let secondHalfOfGroceryList = groceryList.slice(2);
+ console.log(secondHalfOfGroceryList);
+
+ firstHalfOfGroceryList.push("butter");
+
+ groceryList = firstHalfOfGroceryList.concat(secondHalfOfGroceryList);
+ console.log(groceryList);
+
+ groceryList.reverse();
+ console.log(groceryList);
+
+ groceryList.sort();
+ console.log(groceryList);
+ console.log(groceryList.reverse());
+
+ groceryList[3] = "Blueberries";
+ console.log(groceryList);
+ console.log(groceryList.sort()));
+
+let newList = [];
+
+for(let i = 0; i < groceryList.length; i++){
+groceryList[i] = groveryList[i].toLowerCase();
+}
+
+// console.log(groceryList.sort());
+
+groveryList.forEach((Item, index, array) => {
+    array[index] = item.toLowerCase();
+})
+
+    console.log(groceryList);
+
---
+++ js/iterating.js
@@ -0,0 +1,74 @@
+let names = [ "Julia", "Bob", "Harry", "Joe"];
+
+//names.forEach(function(names){
+        console.log(" Present " + names);
+
+            //console.log(names);
+
+    //for(let i = 0; i < names.length; i++) {
+        //console.log(names[i]);
+        // }
+
+function firstItemArray(names){
+        return names[0];
+
+
+    /**
+     * TODO:
+     * Create an array of 4 people's names and store it in a variable called
+     * 'names'.
+     */ 
+        let names = ["Julia", "Bob", "Harry", "Joe"];
+    /**
+     * TODO:
+     * Create a log statement that will log the number of elements in the names
+     * array.
+     */
+        console.log(names.lenght)
+    /**
+     * TODO:
+     * Create log statements that will print each of the names individually by
+     * accessing each element's index.
+     */
+    console.log(names.lenght[0])
+    /**
+     * TODO:
+     * Write some code that uses a for loop to log every item in the names
+     * array.
+     */
+        for(let i = 0; i < names.length; i++){
+            console.log(names[i]);
+        }
+    /**
+     * TODO:
+     * Refactor your above code to use a `forEach` loop
+     */
+        names.forEach(names => console.log(name));
+        names.forEach(function(name){
+            console.log(name);
+        });
+    /**
+     * TODO:
+     * Create the following three functions, each will accept an array and
+     * return an an element from it
+     * - first: returns the first item in the array
+     * - second: returns the second item in the array
+     * - last: returns the last item in the array
+     *
+     * Example:
+     *  > first([1, 2, 3, 4, 5]) // returns 1
+     *  > second([1, 2, 3, 4, 5]) // returns 2
+     *  > last([1, 2, 3, 4, 5]) // return 5
+     */
+        function first(array){
+            return array[0];
+    }
+    
+        function second(array){
+            return array[1];
+        }
+        // Use .lenght to select the last element in the array
+        function third(array){
+            return array[array.length -1];
+        }
+    };
