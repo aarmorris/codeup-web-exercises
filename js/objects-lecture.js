23693d6bf8d669790e1eb607c815905d11bfec1d
Aaron Morris <aaron.r.morris3@gmail.com>
2022-08-22 11:28:54 -0500

    Game Logic

--- js/objects-lecture.js
+++ js/objects-lecture.js
@@ -41,25 +41,70 @@
 // userChoice = "temp";
 //
 
-// let fighter = {
-//     name: "Arata",
-//     hitPoints: 18,
-//     maxDamage: 8,
-//     attack: function(opponent){
-//         console.log(this.name + " attacks!");
-//         let damage = Math.ceil(Math.random() * this.maxDamage);
-//         opponent.hitPoints -= damage;
-//         console.log(this.name + " does " + damage + "points of damage");
-//         console.log(opponent.name + " now has " + opponent.hitPoints + " hit points.");
-//     }
-// }
-//
-// let monster = {
-//     name: "Goblin",
-//     hitPoints: 8,
-//     maxDamage: 6
-// }
+// Let's create a controller object to handle anything players or game objects do that affects the state of the game.  The controller might handle taking user input and calculating the input's effect on the game.
 
+let fighter = {
+    name: "Arata",
+        hitPoints: 18,
+        maxDamage: 8
+}
+
+let monster = {
+    name: "Goblin",
+    hitPoints: 8,
+    maxDamage: 6
+}
+
+
+
+
+let controller = {
+    attack: function(attacker, defender){
+        let defenderHPBeforeAttack = defender.hitPoints;
+        let damage = Math.ceil(Math.random() * attacker.maxDamage);
+        defender.hitPoints -= damage;
+        view.displayAttackResults(attacker, defender, defenderHPBeforeAttack, damage);
+    }
+}
+
+// The view object handles output
+
+let view = {
+    displayAttackResults: function(attacker, defender, defenderHPBeforeAttack, damage){
+        console.log(`${defender.name} has ${defenderHPBeforeAttack} hit points`);
+        console.log(attacker.name + " attacks!");
+        console.log(`${attacker.name} does ${damage} hit points of damage!`);
+        console.log(`${defender.name} now has ${defenderHPBeforeAttack - damage} hit points.`);
+        console.log("-----------------");
+    }
+}
+
+let model = {
+    fighter: {
+        name: "Arata",
+        hitPoints: 18,
+        maxDamage: 8
+    },
+
+    monster: {
+        name: "Goblin",
+        hitPoints: 8,
+        maxDamage: 6
+    }
+}
+
+// fighter.attack(monster);
+// fighter["attack"](monster);
+
+controller.attack(model.fighter, model.monster);
+controller.attack(model.monster, model.fighter);
+
+
+// fighter.attack(monster);
+// fighter["attack"](monster);
+
+controller.attack(fighter, monster);
+controller.attack(monster, fighter);
 
 // Arrays of objects
 
@@ -73,8 +118,9 @@
 //     console.log(`At ${forecast.time} the temperature will be ${forecast.temperature} and feel like ${forecast.feels_like}`);
 // });
 
+// for loop with Arrow syntex
 // hourlyWeather.forEach(forecast =>
-//         cnosole.log(` At ${forecast.time} the temperature will be
+//         cnosole.log(` At ${forecast.time} the temperature will be ${ forecast.temperature} and feel like ${forecast.feels_like}`);
 
 // Array of objects where the objects have proper
 
