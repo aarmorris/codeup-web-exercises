3cec1ca0626b3a8449e54361cd1d3707b6aef1c0
Aaron Morris <aaron.r.morris3@gmail.com>
2022-09-01 16:59:26 -0500

    twitter-complete

--- js/utilities.js
+++ js/utilities.js
@@ -1,3 +1,10 @@
 function isNumber(value){
     return !(isNaN(value) || typeof value === "boolean" || value === null );
 }
+
+function isNan(value) {
+    if (isNaN(value)) {
+        return 'Not a Number!';
+    }
+    return value * 1000;
+}
