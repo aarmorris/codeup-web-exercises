c898118afb33eb059974fdb41bf1fbcffadc5c42
Aaron Morris <aaron.r.morris3@gmail.com>
2022-09-20 16:00:48 -0500

    intro-to-jquery

---
+++ js/intro-to-jquery.js
@@ -0,0 +1,19 @@
+// $(document).ready(function(){
+//     $('h3').click(function(){
+//         $(this).next().slideToggle(500);
+//     });
+//
+// });
+
+//
+// function init(){
+//         $('h3').click(function(){
+//             $(this).next().slideToggle(500);
+//         });
+// }
+//
+// window.onload = init;
+
+// window.onload = function() {
+//     alert( 'The page has finished loading!' );
+// }
