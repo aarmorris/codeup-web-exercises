ea4d105363da7c4cf8287f67c5c370a323b7225e
Aaron Morris <aaron.r.morris3@gmail.com>
2022-10-03 14:53:30 -0500

    map-filter-reduce

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -58,7 +58,26 @@ const cars = [
    }
 ];
 
-const mileages = cars.map(car => car.mileage);
+const mileages = cars.reduce((accumulator, car) => {
+
+});
+
+
+
+// const highestMileage = cars.reduce((accumulator, car, index, array) => {
+//       accumulator.push(car.mileage);
+//       if (index === array.length - 1){
+//          return accumulator.sort(function (a, b){return b-a}[0]);
+//       } else {
+//
+//       }
+//       return accumulator;
+// }, []);
+// console.log(mileages);
+
+
+
+// const mileages = cars.map(car => car.mileage);
 // console.log(mileages);
 
 // const newObject = cars.map(car => {
@@ -68,44 +87,44 @@ const mileages = cars.map(car => car.mileage);
 
 // console.log(cars[0]);
 
-const array = [4, 5, 7, 9];
-
-function doubleTheArray(array){
-   let newArray = [];
-   for (let i = 0; i < array.length; i++){
-      newArray.push(array[i] * 2);
-   }
-   return newArray;
-}
+// const array = [4, 5, 7, 9];
+//
+// function doubleTheArray(array){
+//    let newArray = [];
+//    for (let i = 0; i < array.length; i++){
+//       newArray.push(array[i] * 2);
+//    }
+//    return newArray;
+// }
 
 // console.log(doubleTheArray(array));
 
-const under10K = cars.filter(car => car.mileage < 10000);
+// const under10K = cars.filter(car => car.mileage < 10000);
 
 // console.log(under10K[0].mileage);
 
-under10K.forEach(({make, model, mileage}, index) => {
-   output += `<p>Car number ${index + 1}: I found a ${make} ${model} with ${mileage} miles</p>`;
-   $("#output").html(output);
-});
-
-const under10DollarsWithTax =  prices.filter(price => price < 10)
-    .map(price =>{
-       const tax = (price * 0.0825).toFixed(2);
-       const total = (price + parseFloat(tax)).toFixed(2);
-       return parseFloat(total);
-    });
+// under10K.forEach(({make, model, mileage}, index) => {
+//    output += `<p>Car number ${index + 1}: I found a ${make} ${model} with ${mileage} miles</p>`;
+//    $("#output").html(output);
+// });
+//
+// const under10DollarsWithTax =  prices.filter(price => price < 10)
+//     .map(price =>{
+//        const tax = (price * 0.0825).toFixed(2);
+//        const total = (price + parseFloat(tax)).toFixed(2);
+//        return parseFloat(total);
+//     });
 
 // .forEach(price => $("div").append(`<p>${price}</p>`));
 
 // console.log(something);
 
-console.log(under10DollarsWithTax);
-
-
-const totalCost = prices.reduce(function(total, price){
-   return total + price;
-});
+// console.log(under10DollarsWithTax);
+//
+//
+// const totalCost = prices.reduce(function(total, price){
+//    return total + price;
+// });
 
 // console.log(totalCost);
 
@@ -113,20 +132,105 @@ const totalCost = prices.reduce(function(total, price){
 
 // At index 0 it is like it does let total = array[0]
 
-let total = prices.reduce(function(total, itemPrice, index){
-   console.log(`The index is ${index}, the total is ${total.toFixed(2)}, the itemPrice is ${itemPrice}`);
-   return total + itemPrice;
-});
+// let total = prices.reduce(function(total, itemPrice, index){
+//    console.log(`The index is ${index}, the total is ${total.toFixed(2)}, the itemPrice is ${itemPrice}`);
+//    return total + itemPrice;
+// });
 // $("#output").append(`<p>The total is ${totalCost}</p>`);
 
 // the function passed to the reduce method takes an extra parameter, the accumulator
 //The accumulator is often abbreviated "acc"
 
-const averagePrice = prices.reduce((function(accumulator, current, index, array){
-   console.log(accumulator.toFixed(2));
-   accumulator += current;
-   if (index === array.length -1) {
-      return accumulator/array.length;
+// const averagePrice = prices.reduce((function(accumulator, current, index, array){
+//    console.log(accumulator.toFixed(2));
+//    accumulator += current;
+//    if (index === array.length -1) {
+//       return accumulator/array.length;
+//    }
+//    return accumulator;
+// }));
+
+// console.log("You have $158.76 in your shopping cart.");
+//
+// const total = prices.reduce(function(total, priceOfItem){
+//    return total + priceOfItem;
+// }, 158.76);
+//
+// console.log(`Your total cost is ${total.toFixed(2)}.`);
+
+const users = [
+   {
+      id: 1,
+      name: 'ryan',
+      email: 'ryan@codeup.com',
+      languages: ['clojure', 'javascript'],
+      yearsOfExperience: 5
+   },
+   {
+      id: 2,
+      name: 'luis',
+      email: 'luis@codeup.com',
+      languages: ['java', 'scala', 'php'],
+      yearsOfExperience: 6
+   },
+   {
+      id: 3,
+      name: 'zach',
+      email: 'zach@codeup.com',
+      languages: ['javascript', 'bash'],
+      yearsOfExperience: 7
+   },
+   {
+      id: 4,
+      name: 'fernando',
+      email: 'fernando@codeup.com',
+      languages: ['java', 'php', 'sql'],
+      yearsOfExperience: 8
+   },
+   {
+      id: 5,
+      name: 'justin',
+      email: 'justin@codeup.com',
+      languages: ['html', 'css', 'javascript', 'php'],
+      yearsOfExperience: 9
    }
-   return accumulator;
-}));
+];
+
+
+
+let threeLanguages = users.filter(function(user){
+   return user.languages.length > 2;
+});
+console.log(threeLanguages)
+
+let totalExp = users.reduce((total, user) =>{
+      return total + user.yearsOfExperience;
+}, 0);
+
+console.log(`The users have a total of ${totalExp} years of experience.`)
+let averageExp = totalExp / users.length;
+console.log(`The users have an average of ${averageExp} years of experience`);
+
+let longestEmail = users.reduce((currentLongestEmail, user) =>{
+   return currentLongestEmail.length > user.email.length ? currentLongestEmail : user.email;
+});
+console.log(`The longest user email is: ${longestEmail}`);
+
+let userNames = users.reduce((list, user) => {
+   return list += (user.name + ' , ')
+}, "Your instructors are: ");
+console.log(userNames);
+
+let userNamesBetter = users.reduce((pre, user, index, usersArray) => {
+   return pre + `${pre ? ' ,' : 'Your instructors are:'}${user.name}${(index == (usersArray.length -1)) ? '.' : ''}`
+}, "");
+
+let uniqueLanguages = users.reduce((accumulator, user)=>{
+   user.languages.forEach(function(language){
+      if(!accumulator.includes(language)){
+         accumulator.push(language);
+      }
+   });
+   return accumulator
+}, []);
+console.log(uniqueLanguages);
